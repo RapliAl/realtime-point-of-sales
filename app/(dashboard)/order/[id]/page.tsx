1c161ceea606cece72da1222fd40e528543c1570
MUHAMMAD RAFLI AL ISFAHANI <2311102441245@umkt.ac.id>
2026-02-15 15:14:47 +0800

    feat: add Setup Midtrans for Payment Gateway

--- app/(dashboard)/order/[id]/page.tsx
+++ app/(dashboard)/order/[id]/page.tsx
@@ -1,4 +1,6 @@
 import DetailOrder from "@/app/(dashboard)/order/[id]/_components/detail-order";
+import Script from "next/dist/client/script";
+import {environment} from "@/configs/environment";
 
 export const metadata = {
     title: "Kedai Kita | Menu Management",
@@ -7,5 +9,14 @@ export const metadata = {
 
 export default async function DetailOrderPage({params}: { params: Promise<{ id: string }> }) {
     const {id} = await params;
-    return <DetailOrder id={id}/>
+    return (
+        <div className="w-full">
+            <Script
+                src={`${environment.MIDTRANS_API_URL}/snap/snap.js`}
+                data-client-key={`${environment.MIDTRANS_API_CLIENT_KEY}`}
+                strategy="lazyOnload"
+            />
+            <DetailOrder id={id}/>
+        </div>
+    )
 }
